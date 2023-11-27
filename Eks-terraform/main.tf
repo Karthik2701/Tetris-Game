66da9bafa7421ca566e97cfd7ff124d04fb45c8d
Karthik2701 <102400241+Karthik2701@users.noreply.github.com>
2023-11-26 22:36:15 -0800

    Update main.tf

--- Eks-terraform/main.tf
+++ Eks-terraform/main.tf
@@ -31,7 +31,7 @@ data "aws_subnets" "public" {
     values = [data.aws_vpc.default.id]
   }
 }
-resource "aws_eks_cluster" "example" {
+resource "aws_eks_cluster" "tetris" {
   name     = "tetris-cluster"
   role_arn = aws_iam_role.example.arn
   vpc_config {
