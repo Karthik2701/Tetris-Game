524fc2ece5ae1e2f20ca33ad5e28e04960819e61
Karthik2701 <102400241+Karthik2701@users.noreply.github.com>
2023-11-26 22:30:32 -0800

    Update Main.tf

--- Jenkins-terraform/Main.tf
+++ Jenkins-terraform/Main.tf
@@ -1,77 +1,100 @@
-resource "aws_iam_role" "example_role" {
+resource "aws_iam_role" "role1" {
   name = "Jenkins-terraform"
 
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Principal": {
-        "Service": "ec2.amazonaws.com"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
       },
-      "Action": "sts:AssumeRole"
-    }
-  ]
+    ]
+  })
 }
-EOF
-}
-
-resource "aws_iam_role_policy_attachment" "example_attachment" {
-  role       = aws_iam_role.example_role.name
+resource "aws_iam_role_policy_attachment" "policy-attach" {
+  role       = aws_iam_role.role1.name
   policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
 }
-
-resource "aws_iam_instance_profile" "example_profile" {
+resource "aws_iam_instance_profile" "role-profile" {
   name = "Jenkins-terraform"
-  role = aws_iam_role.example_role.name
+  role = aws_iam_role.role1.name
 }
+resource "aws_security_group" "jenkins-sg" {
+  name        = "Jenkins-sg"
+  description = "open ports for 8080, 22, 443, 80, 9000, 3000"
 
+  ingress {
+    description = "Allow traffic on ports 22, 80, 443, 3000, 8080, and 9000"
+    from_port   = 22
+    to_port     = 9000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    self        = false
+  }
 
-resource "aws_security_group" "Jenkins-sg" {
-  name        = "Jenkins-Security Group"
-  description = "Open 22,443,80,8080,9000"
+  /* ingress = {
+    description = "TLS from VPC"
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-  # Define a single ingress rule to allow traffic on all specified ports
-  ingress = [
-    for port in [22, 80, 443, 8080, 9000, 3000] : {
-      description      = "TLS from VPC"
-      from_port        = port
-      to_port          = port
-      protocol         = "tcp"
-      cidr_blocks      = ["0.0.0.0/0"]
-      ipv6_cidr_blocks = []
-      prefix_list_ids  = []
-      security_groups  = []
-      self             = false
-    }
-  ]
+  ingress = {
+    description = "TLS from VPC"
+    from_port = 443
+    to_port = 443
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
+  ingress = {
+    description = "TLS from VPC"
+    from_port = 9000
+    to_port = 9000
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress = {
+    description = "TLS from VPC"
+    from_port = 8080
+    to_port = 8080
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress = {
+    description = "TLS from VPC"
+    from_port = 3000
+    to_port = 3000
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  } */
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
-  tags = {
-    Name = "Jenkins-sg"
-  }
 }
 
 resource "aws_instance" "web" {
-  ami                    = "ami-0f5ee92e2d63afc18"
+  ami                    = "ami-0fc5d935ebf8bc3bc"
   instance_type          = "t2.large"
-  key_name               = "Mumbai"
-  vpc_security_group_ids = [aws_security_group.Jenkins-sg.id]
-  user_data              = templatefile("./install_jenkins.sh", {})
-  iam_instance_profile   = aws_iam_instance_profile.example_profile.name
+  key_name               = "Devops"
+  vpc_security_group_ids = [aws_security_group.jenkins-sg.id]
+  user_data              = base64encode(file("../jenkins.sh"))
+  iam_instance_profile   = aws_iam_instance_profile.role-profile.name
 
   tags = {
-    Name = "Jenkins-sonar"
+    Name = "Jenkins-Server"
   }
-
   root_block_device {
     volume_size = 30
   }
 }
+  
